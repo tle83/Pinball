6c9ae235882e14bb1ee85a60b921a7449054ec5e
Nevicity <Nevicity@gmail.com>
2017-02-03 23:18:50 -0600

    Bumper added
    Bumpers added to edge of left and right flippers, enabling rotation

---
+++ Assets/Scripts/RightFlipper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightFlipper : MonoBehaviour 
+{
+	HingeJoint2D hinge;
+	private float minAngle;
+	private float maxAngle;
+
+	void start()
+	{
+		hinge = gameObject.GetComponent<HingeJoint2D> ();
+		minAngle = hinge.limits.min;
+		maxAngle = hinge.limits.max;
+
+	}
+
+	void update()
+	{
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			maxAngle = 180; //upper angle
+			minAngle = 90;	//lower angle
+		}
+	}
+}
