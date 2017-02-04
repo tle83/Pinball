b2dce72c820940c9c2eb1a0bd4c2fbc34329f49d
Nevicity <Nevicity@gmail.com>
2017-02-03 20:42:21 -0600

    Initial Uploads
    -Added Flippers, ball, physic properties
    -flipper script still working on

---
+++ Assets/Scripts/flippers.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flipper : MonoBehaviour 
+{
+	Rigidbody2D lFlipper;
+	Rigidbody2D rFlipper;
+
+	public float strength = 30000;
+	public float sustain = 10000;
+
+	void start(){
+		lFlipper = transform.GetChild (1).GetComponent<Rigidbody2D> ();
+		rFlipper = transform.GetChild (0).GetComponent<Rigidbody2D> ();
+	}
+
+	void update(){
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			rFlipper.AddTorque (-strength);
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			lFlipper.AddTorque (strength);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			lFlipper.AddTorque (sustain);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rFlipper.AddTorque (-sustain);
+		}
+	}
+}
