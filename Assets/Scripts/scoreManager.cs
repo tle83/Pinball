7ed59e4423dee3327a0b16cd76ee90f9231e2426
Nevicity <Nevicity@gmail.com>
2017-02-05 17:17:30 -0600

    Revert "Revert "Score Counter Added""
    This reverts commit 20263d8dfa01c6949708130c783b64d98038d2cc.

---
+++ Assets/Scripts/scoreManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreManager : MonoBehaviour 
+{
+	private int count;
+	public Text countText;
+
+	void Start()
+	{
+		count = 0;
+		SetCountText ();
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.CompareTag("Pins"))
+		{
+			count += 10;
+			UnityEngine.Debug.Log ("Score: " + count);
+			SetCountText ();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Score: " + count.ToString ();
+	}
+
+}
