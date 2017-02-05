5c28579ee010fd7a6a5a0c6727c4cf8116ab47e3
Nevicity <Nevicity@gmail.com>
2017-02-04 18:48:53 -0600

    New Art Assests
    =Added new walls
    =Unfinish scoreboard

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+	public UIManager ui;
+
+	void Start () {
+		ui = GameObject.FindWithTag ("UI").GetComponent<UIManager>();
+	}
+	
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Pins") {
+			ui.IncScore ();
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+	int score = 0;
+	public Text scoreText;
+
+	void Start(){
+	}
+
+	void Update(){
+	}
+
+	public void IncScore(){
+		score += 20;
+		scoreText.text = "Score: " + score;
+	}
+
+	void OnGUI(){
+		GUI.Box (new Rect (10, 10, 200, 90), "SCORE: ");
+	}
+}
